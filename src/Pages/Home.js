4ef93c10e0b6f65a90cebaf34c277b1d43222120
mohan <mohan2492003@gmail.com>
2026-02-08 15:35:29 +0530

    Normalize line endings

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,27 +1,34 @@
 import { Fragment, useEffect, useState } from "react";
 import Productcard from "../Components/ProductCart";
 import { useSearchParams } from "react-router-dom";
-// import productsmodel from "../../../backend/Models/productModel";
+import axios from "axios";
 
 export default function Home() {
-    const [products , setproduct] = useState([]);
-    const [searchParams ,  setSearchParams] = useSearchParams()
-    useEffect (() => {
-   fetch(process.env.REACT_APP_API_URL+'/product?'+searchParams.toString())
-   .then(res => res.json())
-   .then(res => setproduct(res.product || []));
-    },[searchParams])
+  const [products, setproduct] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    axios
+      .get(
+        process.env.REACT_APP_API_URL +
+          "/api/v1/products?" +
+          searchParams.toString()
+      )
+      .then(res => setproduct(res.data.product || []))
+      .catch(err => console.error(err));
+  }, [searchParams]);
+
   return (
     <Fragment>
       <h1 id="products_heading">Latest Products</h1>
 
       <section id="products" className="container mt-5">
         <div className="row">
-            {products.map(product => (<Productcard  product={product}/>))}
-         {/* <Productcard/> */}
+          {products.map(product => (
+            <Productcard product={product} />
+          ))}
         </div>
       </section>
     </Fragment>
   );
 }
-
